8ac5b0fc40231f256b94d5436a4cfbe89ce45df2
Aseem5047 <aseemgupta2002@gmail.com>
2023-11-27 20:16:41 +0530

    final changes

--- api/index.js
+++ api/index.js
@@ -35,10 +35,10 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser())
 
 // Handle preflight requests
-// origin: ['http://127.0.0.1:5173', 'https://crowdvista.vercel.app'],
+// origin: 'https://crowdvista.vercel.app',
 app.use(cors({
     credentials: true,
-    origin: 'https://crowdvista.vercel.app',
+    origin: ['http://127.0.0.1:5173', 'https://crowdvista.vercel.app'],
 }))
 
 dotenv.config();
